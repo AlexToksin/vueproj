ffa17c3fab243c8afd9775fc5b04ea4659a6141c
alextoksin <griffin-sk@mail.ru>
2019-08-17 22:42:23 +0400

    header 95% completed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,21 +9,25 @@
         <div class="header header__text header__text_small">Craft By Css author.com</div>
       </div>
     </div>
-    <div class="header header__nav-wraper">
-      <div class="header header__shadow header__shadow_one"></div>
-      <div class="header header__shadow header__shadow_two"></div>
-      <div class="header header__nav">
-        <div class="header header__button">
+    <div class="main-nav-wrapper">
+      <div class="main-nav__shadow main-nav__shadow_one"></div>
+      <div class="main-nav__shadow main-nav__shadow_two"></div>
+      <div class="main-nav">
+        <div class="main-nav__button">
           <img src="../assets/about.png" alt="about" class="logo">
-          <div class="header__btn header__caption">About us</div>
+          <div class="main-nav__button-caption">About us</div>
         </div>
-        <div class="header header__button">
-          <img src="../assets/service.png" alt="service" class="logo">
-          <div class="header__btn header__caption">Service</div>
+        <div class="main-nav__button main-nav__button_content">
+          <div class="main-nav__button-border-left"></div>
+          <div class="main-nav__button-wrapper">
+            <img src="../assets/service.png" alt="service" class="logo">
+            <div class="main-nav__button-caption">Service</div>
+          </div>
+          <div class="main-nav__button-border-right"></div>
         </div>
-        <div class="header header__button">
+        <div class="main-nav__button">
           <img src="../assets/contact.png" alt="contats" class="logo">
-          <div class="header__btn header__caption">Contacts</div>
+          <div class="main-nav__button-caption">Contacts</div>
         </div>
       </div>
     </div>
